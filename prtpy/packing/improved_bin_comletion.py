56df53cc62bfd82ab94404081889d71562fd1a9d
ishay320 <ishay320@hotmail.com>
2022-04-17 19:16:24 +0300

    init IBC algorithm

---
+++ prtpy/packing/improved_bin_comletion.py
@@ -0,0 +1,31 @@
+"""
+    Implementations of Improved bin completion algorithm:
+        https://www.ijcai.org/Proceedings/13/Papers/103.pdf
+
+    Ethan L. Schreiber and Richard E. Korf "Improved Bin Completion for Optimal Bin Packing and Number Partitioning"
+
+"""
+
+from typing import Callable, List, Any
+from prtpy import outputtypes as Bins
+
+
+def bin_packing(
+    bins: Bins,
+    binsize: int,
+    items: List[Any],
+    valueof: Callable[[Any], float] = lambda x: x,
+):
+    """
+    Pack the given items into bins using the Improved Bin Comletion algorithm.
+    The IBC algorithm pack the items to the fewest number of bins sized binsize.
+
+    >>> from prtpy.bins import BinsKeepingContents
+    >>> bin_packing(BinsKeepingContents(), binsize=9, items=[1,2,3,3,5,9,9]).bins
+    [[1, 2, 3, 3], [5], [9], [9]]
+    >>> bin_packing(BinsKeepingContents(), binsize=18, items=[1,2,3,3,5,9,9]).bins
+    [[1, 2, 3, 3, 5], [9, 9]]
+    >>> list(bin_packing(BinsKeepingContents(), binsize=9, items=[1,2,3,3,5,9,9]).sums)
+    [9.0, 5.0, 9.0, 9.0]
+    """
+    assert("Not implemented yet")
