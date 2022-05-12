b12a61d5d205aea82c29b0e47d01a4720fac2519
samuelharroch <samuel@optival.com>
2022-05-12 14:54:53 +0300

    kk and ckk declarations

---
+++ prtpy/partitioning/kk.py
@@ -0,0 +1,7 @@
+
+from typing import Callable, List, Any
+from prtpy import outputtypes as out, objectives as obj, Bins
+
+
+def kk(bins: Bins, items: List[any], valueof: Callable=lambda x: x) -> Bins:
+    pass
