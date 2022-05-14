7f78d2c08a822f71ffb10867494ffdaaff8e77e4
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-05-15 00:42:42 +0300

    almost done kk and ckk algorithms

--- prtpy/partitioning/kk.py
+++ prtpy/partitioning/kk.py
@@ -10,10 +10,15 @@
     Author: Kfir Goldfarb
     Date: 26/04/2022
     Email: kfir.goldfarb@msmail.ariel.ac.il
-"""
 
-from typing import Callable, List, Any
-from prtpy import outputtypes as out, objectives as obj, Bins
+    - help -
+    https://en.wikipedia.org/wiki/Largest_differencing_method#:~:text=The%20complete%20Karmarkar%E2%80%93Karp%20algorithm,constructing%20a%20tree%20of%20degree&text=In%20the%20case%20k%3D2,them%20in%20the%20same%20set).
+    http://web.cecs.pdx.edu/~bart/cs510ai/papers/korf-ckk.pdf
+
+"""
+import copy
+from typing import Callable, List, Any, Dict, Union
+from prtpy import outputtypes as out, objectives as obj, Bins, BinsKeepingContents
 
 
 def kk(bins: Bins, items: List[any], valueof: Callable = lambda x: x):
@@ -21,23 +26,69 @@ def kk(bins: Bins, items: List[any], valueof: Callable = lambda x: x):
     Partition the numbers using the Karmarkar-Karp Heuristic partitioning algorithm
 
     >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> kk(BinsKeepingContents(2), items=[1, 6, 2, 3, 4, 1, 7, 6, 4]).bins
-    [[4, 7, 6], [1, 2, 1, 3, 4, 6]]
+    >>> kk(BinsKeepingContents(2), items=[1, 6, 2, 3, 7, 4, 5, 8]).bins
+    [[8, 5, 3, 2], [7, 6, 4, 1]]
 
-    >>> kk(BinsKeepingContents(3), items=[4, 5, 6, 7, 8]).bins
-    [[8], [4, 7], [5, 6]]
+    >>> kk(BinsKeepingContents(2), [1, 2, 3, 4, 5, 6]).bins
+    [[5, 3, 2], [6, 4, 1]]
+
+    >>> list(kk(BinsKeepingContents(2), items=[4, 5, 6, 7, 8]).bins)
+    [[8, 6], [7, 5, 4]]
 
     >>> list(kk(BinsKeepingContents(2), items=[18, 17, 12, 11, 8, 2]).sums)
-    [36.0, 32.0]
+    [37.0, 31.0]
 
-    >>> from prtpy import partition
-    >>> partition(algorithm=kk, numbins=3, items={"a":1, "b":2, "c":3, "d":3, "e":5, "f":9, "g":9})
-    [['f', 'b'], ['g', 'a'], ['e', 'c', 'd']]
-    >>> partition(algorithm=kk, numbins=2, items={"a":1, "b":2, "c":3, "d":3, "e":5, "f":9, "g":9}, outputtype=out.Sums)
+    #>>> from prtpy import partition
+    #>>> partition(algorithm=kk, numbins=3, items={"a":1, "b":2, "c":3, "d":3, "e":5, "f":9, "g":9})
+    #[['f', 'b'], ['g', 'a'], ['e', 'c', 'd']]
+    #>>> partition(algorithm=kk, numbins=2, items={"a":1, "b":2, "c":3, "d":3, "e":5, "f":9, "g":9}, outputtype=out.Sums)
     array([16., 16.])
 
     """
-    pass
+    values = None
+    if type(items) == list:
+        input_items = [i for i in items]
+    else:
+        input_items = list(items)
+        values = [i for i in items.values()]
+
+    original_items = [i for i in input_items]
+    difference_sets = [[i for i in original_items]]
+    while len(input_items) > 1:
+        max_a = max(input_items)
+        input_items.remove(max_a)
+        max_b = max(input_items)
+        input_items.remove(max_b)
+        diff = abs(max_a - max_b)
+        input_items.append(diff)
+        input_items.sort()
+        input_items.reverse()
+        difference_sets.append(copy.copy(input_items))
+
+    A, B = [], []
+    difference_sets.reverse()
+    while len(difference_sets) > 0:
+        difference_set = difference_sets[0]
+        while len(difference_set) > 0:
+            integer = max(difference_set)
+            if integer not in A and integer not in B:
+                if sum(A) < sum(B):
+                    A.append(integer)
+                else:
+                    B.append(integer)
+            difference_set.remove(integer)
+        difference_sets.remove(difference_set)
+
+    for i in reversed(A):
+        if i in original_items:
+            bins.add_item_to_bin(item=i, bin_index=0)
+
+    for i in reversed(B):
+        if i in original_items:
+            bins.add_item_to_bin(item=i, bin_index=1)
+
+    # print(bins)
+    return bins
 
 
 if __name__ == "__main__":
