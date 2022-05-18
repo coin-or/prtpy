6d0ea42e2cb671b1770f1cb30fb489a0390cd5eb
avsha114 <avsha114@gmail.com>
2022-05-18 11:49:56 +0300

    fixed algorithm

--- prtpy/packing/bc_utilities.py
+++ prtpy/packing/bc_utilities.py
@@ -2,7 +2,8 @@
 These functions are being used by the Bin-Completion algorithm for solving the bin packing problem.
 See "bin_completion.py" for more details.
 """
-
+import copy
+import functools
 from typing import List
 from prtpy import outputtypes as out, Bins, BinsKeepingContents
 
@@ -41,12 +42,90 @@ def l2_lower_bound(binsize: float, items: List) -> float:
 
     return (estimated_waste + total_sum) / binsize
 
+def find_undominated_pairs(x: int, y: int, items: List, binsize: int) -> List:
+    start = 0;
+    end = len(items)-1
+    undominated_pairs = []
 
-class NodeBin:
-    def __init__(self, items: List, level: int = 0, prev=None):
-        self.items = items.copy()
-        self.prev = prev
-        self.next_bins = BinsKeepingContents()
-        self.level = level
+    while (start > end):
+        sum = items[start] + items[end]
+        if x + sum > binsize:
+            start += 1
+        elif sum <= y:
+            end -= 1
+        else:
+            undominated_pairs.append([items[start], items[end]])
+            start += 1
+            end -= 1
 
+    return undominated_pairs
+
+def generate_undominated_bin_completions(items: List, binsize: int) -> List:
+    x = items[0]
+
+    if len(items) == 1 or x + items[-1] > binsize:
+        return []
+    if len(items) == 2:
+        return [[items[1]]]
+
+    y = items[1]
+    for i in range(2, len(items) - 1):
+        if x + y > binsize:
+            y = items[i]
+        else:
+            break
+
+    items.remove(x)
+    items.remove(y)
+    undominated_pairs = find_undominated_pairs(x, y, items, binsize)
+    if not undominated_pairs:
+        return [[y]]
+    else:
+        return undominated_pairs
+
+
+# This is a recursive function which fills the bins.
+# See comments below.
+def fill_bins(items: List, bins: Bins, bin_index: int, binsize: int, bound: int) -> tuple[Bins, bool]:
+    # if items is empty, and we finished with fewer bins than the old bound, solution was found
+    # if we reached the same amount of bins or more - return false
+    if not items:
+        if bins.num <= bound:
+            return bins, True
+        else:
+            return bins, False
+
+    # if we are reached the same amount of bins or more - return false
+    if bins.num >= bound:
+        return bins, False
+
+    bins.add_empty_bins()
+    # Add the largest item to the bin and generate possible completions for that bin
+    bins.add_item_to_bin(items[0], bin_index)
+    possible_bin_completions = generate_undominated_bin_completions(items, binsize)
+
+    # if no possible completions was found - fill the next bin
+    if not possible_bin_completions:
+        return fill_bins(items, bins, bin_index + 1, binsize, bound)
+
+    # if len(possible_bin_completions) == 1:
+    #     new_items = [element for element in items if element not in possible_bin_completions[0]]
+    #     new_bins = copy.deepcopy(bins)
+    #     map(functools.partial(bins.add_item_to_bin, bin_index=bin_index), possible_bin_completions[0])
+    #
+    #     return fill_bins(new_items, new_bins, bin_index + 1, binsize, bound)
+
+    # Go through all possible completions,
+    # for each option - try to fill the next bins like a branch
+    # if the solution found was not valid - move on to the next completion
+    for comp in sorted(possible_bin_completions, key=sum, reverse=True):
+        new_items = [element for element in items if element not in comp]
+        new_bins = copy.deepcopy(bins)
+        map(functools.partial(bins.add_item_to_bin, bin_index=bin_index), comp)
+        solution, valid = fill_bins(new_items, new_bins, bin_index + 1, binsize, bound)
+
+        if valid:
+            return solution, valid
+
+    return bins, False
 
--- prtpy/packing/bin_completion.py
+++ prtpy/packing/bin_completion.py
@@ -1,5 +1,3 @@
-import copy
-import functools
 from typing import Callable, List, Any
 from prtpy import outputtypes as out, Bins, BinsKeepingContents
 from prtpy.packing import first_fit
@@ -22,90 +20,77 @@ def bin_completion(
     >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
 
     Example 1: max capacity
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[100,100,100,100,100,100])
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[100,100,100,100,100,100]).bins
     [[100], [100], [100], [100], [100], [100]]
 
     Example 2: min capacity
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[1,2,3,4,5,85])
-    [[1,2,3,4,5,85]]
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[1,2,3,4,5,85]).bins
+    [[85, 5, 4, 3, 2, 1]]
 
     Example 3: Complex input
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[99,94,79,64,50,44,43,37,32,19,18,7,3])
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[99,94,79,64,50,44,43,37,32,19,18,7,3]).bins
     [[99], [94,6], [79,18,3], [64,32], [50,43,7], [44,37,19]]
 
     Example 4: Article Example #1
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[100, 98, 96, 93, 91, 87, 81, 59, 58, 55, 50, 43, 22, 21, 20, 15, 14, 10, 8, 6, 5, 4, 3, 1, 0])
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[100, 98, 96, 93, 91, 87, 81, 59, 58, 55, 50, 43, 22, 21, 20, 15, 14, 10, 8, 6, 5, 4, 3, 1, 0]).bins
     [[100], [98, 1], [96, 4], [93, 6], [91, 8], [87, 10, 3], [81, 14, 5], [59, 21, 20], [58, 22, 15], [55, 43], [50]]
 
     Example 5: Article Example #2
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[6, 12, 15, 40, 43, 82])
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[6, 12, 15, 40, 43, 82]).bins
     [[82,6,12], [15,40,43]]
 
     Example 6: Article Example #3
-    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[99, 97, 94, 93, 8, 5, 4, 2])
+    >>> bin_completion(BinsKeepingContents(), binsize=100, items=[99, 97, 94, 93, 8, 5, 4, 2]).bins
     [[99], [97, 2], [94, 5], [93, 4], [8]]
     """
 
-    ffd_solution = first_fit.decreasing(BinsKeepingContents(), binsize, items)
-    lower_bound = l2_lower_bound(binsize, items)
+    # Find the FFD solution and check if it's optimal using the L2 lower bound
+    ffd_solution = first_fit.decreasing(BinsKeepingContents(), binsize, items.copy())
+    lower_bound = l2_lower_bound(binsize, items.copy())
 
-    # Check casting
     if ffd_solution.num == math.ceil(lower_bound):
         return ffd_solution
 
     best_solution_so_far = ffd_solution
-    # possible_partial_solutions = []
-    bin_index = 0
 
     sorted_items = sorted(items, reverse=True)
-    bin_tree = NodeBin(items)
 
-    for x in sorted_items:
-        bins.add_empty_bins()
-        bins.add_item_to_bin(sorted_items.pop(0), bin_index)
+    # fill the bins to find a solution
+    solution, valid = fill_bins(sorted_items, bins, 0, binsize, lower_bound)
 
-        current_bin_completions = find_undominated_completions()
-
-        if len(current_bin_completions) == 1:
-            map(functools.partial(bins.add_item_to_bin, bin_index=bin_index), current_bin_completions[0])
-            bin_index += 1
-        else:
-            chosen_completion = sorted(current_bin_completions, key=sum).pop()
-            map(functools.partial(bins.add_item_to_bin, bin_index=bin_index), chosen_completion)
-            possible_partial_solutions.extend(current_bin_completions)
-
-
-
-
-    return 0
+    return solution
 
 
 if __name__ == "__main__":
     import doctest
 
-    items = [99, 97, 94, 93, 8, 5, 4, 2]
-    s = sum(items)
-    print(l2_lower_bound(100, items))
-    print("sum ", s)
-    print("sum+98 ", (98 + sum(items)))
-    print("Should be ", ((98 + sum(items)) / 100))
+    # items = [99, 97, 94, 93, 8, 5, 4, 2]
+    # s = sum(items)
+    # print(l2_lower_bound(100, items))
+    # print("sum ", s)
+    # print("sum+98 ", (98 + sum(items)))
+    # print("Should be ", ((98 + sum(items)) / 100))
+    #
+    # from prtpy.packing import first_fit as ff
+    #
+    # items = [100, 100, 100, 100, 100, 100]
+    # print(ff.decreasing(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3]))
+    # print()
+    # print(bin_completion(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3]))
+    #
+    # def f(x, y):
+    #     return x+y
+    #
+    #
+    # m = map(functools.partial(f, y=10), items)
+    #
+    # print(list(m))
+    #
+    # binstest = ff.decreasing(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3])
+    #
+    # print("TEST")
+    # print()
+    # items = [99,94,79,64,50,44,43,37,32,19,18,7,3]
+    # print(bin_completion(BinsKeepingContents(), 100, items))
 
-    from prtpy.packing import first_fit as ff
-
-    items = [100, 100, 100, 100, 100, 100]
-    print(ff.decreasing(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3]))
-    print()
-    print(bin_completion(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3]))
-
-    def f(x, y):
-        return x+y
-
-
-    m =map(functools.partial(f, y=10), items)
-
-    print(list(m))
-
-    binstest = ff.decreasing(BinsKeepingContents(), binsize=100, items=[99, 94, 79, 64, 50, 44, 43, 37, 32, 19, 18, 7, 3])
-
-
-    # print(doctest.testmod())
+    print(doctest.testmod())
