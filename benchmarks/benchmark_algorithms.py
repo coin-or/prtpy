1404623f10164929fab1f8c09ebe4780e885b123
Erel Segal-Halevi <erelsgl@gmail.com>
2022-05-25 13:35:16 +0300

    add benchmarks

--- benchmarks/benchmark_algorithms.py
+++ benchmarks/benchmark_algorithms.py
@@ -1,25 +1,18 @@
 import prtpy, benchmark, logging
 from matplotlib import pyplot as plt
-from partition_random import partition_random_items
-
-algorithm = prtpy.partitioning.ilp
-numbins = 4
+from partition_uniform_integers import partition_random_items
 
 benchmark.logger.setLevel(logging.INFO)
 benchmark.logger.addHandler(logging.StreamHandler())
-
 
 results = benchmark.find_max_solvable_size(
     partition_random_items,
     "numitems",
     range(1, 101, 10),
     max_time_in_seconds=10,
-    algorithm=algorithm,
-    numbins=numbins,
-    minvalue=1,
-    maxvalue=100,
-    objective=prtpy.obj.MaximizeSmallestSum,
-    outputtype=prtpy.out.SmallestSum,
+    algorithm=prtpy.partitioning.greedy,
+    numbins=4,
+    bitsperitem=7,
 )
 
 results.plot(plt)
