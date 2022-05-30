920ddbff2ce299bc0031f0868424ec9c9dfa6277
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-05-30 15:17:17 +0300

    update ckk

--- prtpy/partitioning/ckk.py
+++ prtpy/partitioning/ckk.py
@@ -1,5 +1,5 @@
 """
-    Partition the numbers using the complete Karmarkar-Karp Heuristic partitioning algorithm
+    Partition the items using the complete Karmarkar-Karp Heuristic partitioning algorithm
     Taken help from:
 
     Taken from the "A Hybrid Recursive Multi-Way Number Partitioning Algorithm (2011)" Paper
@@ -11,167 +11,108 @@
     Date: 26/04/2022
     Email: kfir.goldfarb@msmail.ariel.ac.il
 """
-import copy
-
+import heapq
 import numpy as np
 from typing import Callable, List
-from prtpy import outputtypes as out, Bins, BinsKeepingContents
+from itertools import count, permutations
+from prtpy import Bins, BinsKeepingContents
 
 
 def ckk(bins: Bins, items: List[any], valueof: Callable = lambda x: x):
     """
-    Partition the numbers using the complete Karmarkar-Karp Heuristic partitioning algorithm
+    Partition the items using the complete Karmarkar-Karp Heuristic partitioning algorithm
 
     >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
+    >>> ckk(BinsKeepingContents(1), items=[1, 6, 2, 3, 4, 7]).bins
+    [[1, 6, 2, 3, 4, 7]]
+
     >>> ckk(BinsKeepingContents(2), items=[95, 15, 75, 25, 85, 5]).bins
-    [[95, 25, 15], [85, 75, 5]]
+    [[85, 75], [95, 25, 15, 5]]
 
     >>> ckk(bins=BinsKeepingContents(2), items=[5, 8, 6, 4, 7]).bins
-    [[8, 6], [7, 5, 4]]
+    [[7, 5, 4], [8, 6]]
 
     >>> list(ckk(BinsKeepingContents(2), items=[1, 6, 2, 3, 4, 7]).sums)
     [12.0, 11.0]
 
     >>> ckk(BinsKeepingContents(2), items=[95, 15, 75, 25, 85, 5]).bins
-    [[95, 25, 15, 5], [85, 75]]
+    [[85, 75], [95, 25, 15, 5]]
 
-    >>> ckk(bins=BinsKeepingContents(2), items=[5, 8, 6, 4, 7]).bins
-    [[8, 7], [6, 5, 4]]
+    >>> list(ckk(BinsKeepingContents(3), items=[8, 7, 6, 5, 4]).bins)
+    [[7, 4], [6, 5], [8]]
+
+    >>> list(ckk(BinsKeepingContents(3), items=[95, 15, 75, 25, 85, 5]).bins)
+    [[95, 5], [85, 15], [75, 25]]
 
     """
     k = bins.num
     if k == 0:
         return bins
     elif k == 1:
-        return [bins.add_item_to_bin(item=item, bin_index=0) for item in items]
-
-    S = []
-    items.sort(reverse=True, key=valueof)
-    for i in items:
-        s = [[i]]
-        s = filled_empy_lists(s, k)
-        S.append(s)
-
-    while len(S) > 2:
-        A = S[0]
-        B = S[1]
-        C = combine(A, B, k)
-        S.remove(A)
-        S.remove(B)
-        S.append(C)
-
-    result = combine(S[0], S[1], k)
-
-    for index, i in enumerate(result):
-        for item in i:
-            bins.add_item_to_bin(item=item, bin_index=index)
-
+        for item in items:
+            bins.add_item_to_bin(item=item, bin_index=0)
+        return bins
+    items = sorted(items, key=valueof)
+    partition = heuristic(items=items, k=k)
+    result = [p[0] for p in partition]
+    for index, p in enumerate(sorted(result[0], key=sum, reverse=True)):
+        for n in sorted(p, reverse=True):
+            bins.add_item_to_bin(item=n, bin_index=index)
     return bins
 
 
-def remove_from_nested_list(s: list, rest: list):
-    for i in range(len(s)):
-        if i < len(s) and s[i] == rest:
-            s.pop(i)
+def heuristic(items: List[any], k: int = 2):
+    stack = [[]]
+    heap_count = count()
+    for number in items:
+        l: List[List[int]] = [[] for _ in range(k - 1)]
+        r: List[List[int]] = [[number]]
+        this_partition = l + r
+        this_sizes: List[int] = [0] * (k - 1) + [number]
+        heapq.heappush(
+            stack[0], (-number, next(heap_count), this_partition, this_sizes)
+        )
+    best = -np.inf
+    while stack:
+        partitions = stack.pop()
+        if _possible_partition_difference_lower_bound(partitions, k) <= best:
+            continue
+        if len(partitions) == 1:
+            num = partitions[0][0]
+            if num > best:
+                best = num
+                _, _, final_partition, final_sums = partitions[0]
+                yield final_partition, final_sums
+                if num == 0:
+                    return
+            continue
+        _, _, p1, p1_sum = heapq.heappop(partitions)
+        _, _, p2, p2_sum = heapq.heappop(partitions)
+        tmp_stack_extension = []
+        for new_partition, new_sizes in _combine_partitions(p1, p2):
+            tmp_partitions = partitions[:]
+            diff = max(new_sizes) - min(new_sizes)
+            heapq.heappush(
+                tmp_partitions, (-diff, next(heap_count), new_partition, new_sizes)
+            )
+            tmp_stack_extension.append(tmp_partitions)
+        stack.extend(sorted(tmp_stack_extension))
 
 
-def get_max_sum(s: list) -> list:
-    sum_of_list = 0
-    result = []
-    for i in s:
-        if sum(i) > sum_of_list and len(i) > 0:
-            sum_of_list = sum(i)
-            result = i
-    return result
+def _combine_partitions(partition_1, partition_2):
+    yielded = set()
+    for permutation in permutations(partition_1, len(partition_1)):
+        out = sorted(sorted(p + l) for p, l in zip(permutation, partition_2))
+        out_ = tuple(tuple(el) for el in out)
+        if out_ not in yielded:
+            yielded.add(out_)
+            yield out, [sum(x) for x in out]
 
 
-def get_min_sum(s: list) -> list:
-    sum_of_list = np.inf
-    result = []
-    for i in s:
-        if sum(i) < sum_of_list and len(i) > 0:
-            sum_of_list = sum(i)
-            result = i
-    return result
-
-
-def delete_empty_lists(s: list) -> list:
-    result = []
-    for i in s:
-        for j in i:
-            if sum(j) != 0:
-                result.extend(j)
-    return s
-
-
-def union(a: list, b: list, valueof: Callable = lambda x: x) -> list:
-    return sorted(list(set(a)) + list(set(b)), reverse=True, key=valueof)
-
-
-def filled_empy_lists(s: list, n: int):
-    result = copy.copy(s)
-    result.extend([[] for _ in range(n - 1)])
-    return result
-
-
-def len_filled_list(s: list):
-    length = 0
-    for i in s:
-        if len(i) != 0:
-            length += 1
-    return length
-
-
-def combine(a: list, b: list, n: int, valueof: Callable = lambda x: x):
-    """
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> combine(a=[[95], [85]], b=[[75], [25]], n=2)
-    [[95, 25], [85, 75]]
-
-    >>> combine(a=[[9], [7]], b=[[3]], n=2)
-    [[7, 3], [9]]
-
-    >>> combine(a=[[9], [7]], b=[[20]], n=2)
-    [[9, 7], [20]]
-
-    """
-    a.sort(reverse=True, key=valueof)
-    b.sort(reverse=True, key=valueof)
-    result = []
-    a_length = len_filled_list(a)
-    b_length = len_filled_list(b)
-    if a_length > b_length:
-        if n <= 2:
-            max_of_a = get_max_sum(a)
-            min_of_a = get_min_sum(a)
-            num_of_b = b[0]
-            if num_of_b < min_of_a:
-                result = [union(min_of_a, num_of_b), max_of_a]
-            else:
-                result = [union(max_of_a, min_of_a), num_of_b]
-        # else:
-        #     pass
-    elif a_length < b_length:
-        return combine(b, a, n)
-    elif a_length == 1 and b_length == 1:
-        result = [a[0], b[0]]
-    else:
-        rest = 0
-        if n > 2:
-            rest = max(get_max_sum(a), get_max_sum(b))
-            remove_from_nested_list(a, rest)
-            remove_from_nested_list(b, rest)
-
-        max_of_a = get_max_sum(a)
-        min_of_a = get_min_sum(a)
-        max_of_b = get_max_sum(b)
-        min_of_b = get_min_sum(b)
-
-        if n > 2:
-            result = [union(max_of_a, min_of_b), union(max_of_b, min_of_a), rest]
-        else:
-            result = [union(max_of_a, min_of_b), union(max_of_b, min_of_a)]
-    return result
+def _possible_partition_difference_lower_bound(node, k: int) -> int:
+    sizes_flattened = [size for partition in node for size in partition[3]]
+    max_sizes_flattened = max(sizes_flattened)
+    return -(max_sizes_flattened - (sum(sizes_flattened) - max_sizes_flattened) // (k - 1))
 
 
 if __name__ == "__main__":
