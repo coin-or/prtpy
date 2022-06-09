22b917a48cf31706360e884ecf6b47a58112964f
samuelharroch <samuel@optival.com>
2022-06-09 17:11:42 +0300

    remove sites files

--- our_flask/flask_example/__init__.py
+++
@@ -1,7 +0,0 @@
-# Here, we add a secret key:
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'ecf6e975838a2f7bf3c5dbe7d55ebe5b'  ###
-from flask_example import routes
--- our_flask/run.py
+++
@@ -1,2 +0,0 @@
-from flask_example import app
-app.run(debug = True)
