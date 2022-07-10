fb54a66430316fb83babf778ce647bea88e44b4d
Erel Segal-Halevi <erelsgl@gmail.com>
2022-07-10 23:45:41 +0300

    rr binner

--- prtpy/partitioning/roundrobin.py
+++ prtpy/partitioning/roundrobin.py
@@ -6,23 +6,22 @@ Since: 2022-02
 """
 
 from typing import Callable, List, Any
-from prtpy import outputtypes as out, objectives as obj, Bins
+from prtpy import outputtypes as out, objectives as obj, Binner
 
 
-def roundrobin(
-    bins: Bins,
-    items: List[any],
-    valueof: Callable[[Any], float] = lambda x: x,
-):
+def roundrobin(binner: Binner, numbins: int, items: List[any]):
     """
     Partition the given items using the roundrobin number partitioning algorithm.
 
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> roundrobin(BinsKeepingContents(2), items=[1,2,3,3,5,9,9]).bins
-    [[9, 5, 3, 1], [9, 3, 2]]
-    >>> roundrobin(BinsKeepingContents(3), items=[1,2,3,3,5,9,9]).bins
-    [[9, 3, 1], [9, 3], [5, 2]]
-    >>> list(roundrobin(BinsKeepingContents(3), items=[1,2,3,3,5,9,9]).sums)
+    >>> from prtpy import BinnerKeepingContents, BinnerKeepingSums, printbins
+    >>> printbins(roundrobin(BinnerKeepingContents(), 2, items=[1,2,3,3,5,9,9]))
+    Bin #0: [9, 5, 3, 1], sum=18.0
+    Bin #1: [9, 3, 2], sum=14.0
+    >>> printbins(roundrobin(BinnerKeepingContents(), 3, items=[1,2,3,3,5,9,9]))
+    Bin #0: [9, 3, 1], sum=13.0
+    Bin #1: [9, 3], sum=12.0
+    Bin #2: [5, 2], sum=7.0
+    >>> list(roundrobin(BinnerKeepingSums(), 3, items=[1,2,3,3,5,9,9]))
     [13.0, 12.0, 7.0]
 
     >>> from prtpy import partition
@@ -32,9 +31,10 @@ def roundrobin(
     [18.0, 14.0]
     """
     ibin = 0
-    for item in sorted(items, key=valueof, reverse=True):
-        bins.add_item_to_bin(item, ibin)
-        ibin = (ibin+1) % bins.num
+    bins = binner.new_bins(numbins)
+    for item in sorted(items, key=binner.valueof, reverse=True):
+        binner.add_item_to_bin(bins, item, ibin)
+        ibin = (ibin+1) % numbins
     return bins
 
 
