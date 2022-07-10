69517d4332e3a5d31dc5aeaa4a185a33ec4f5c09
Erel Segal-Halevi <erelsgl@gmail.com>
2022-07-10 22:16:03 +0300

    ckk binner

--- prtpy/partitioning/complete_karmarkar_karp_sy.py
+++ prtpy/partitioning/complete_karmarkar_karp_sy.py
@@ -11,6 +11,7 @@ Date:    2022-03
 from typing import Iterator, List, Tuple, Callable, List, Any
 from prtpy import outputtypes as out, objectives as obj, Bins, Binner, BinsKeepingContents, BinsKeepingSums
 import logging, numpy as np
+from prtpy.binners import BinsArray
 
 from prtpy.partitioning.karmarkar_karp_sy import BinsSortedByMaxDiff
 
@@ -33,30 +34,30 @@ def _possible_partition_difference_lower_bound(current_heap: List[Tuple[int, int
 
 
 
-def optimal(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, binner:Binner=None, numbins:int=None) -> Bins:
+def optimal(binner: Binner, numbins: int, items: List[any]) -> BinsArray:
     """
     Finds a partition in which the largest sum is minimal, using the Complete Greedy algorithm.
 
     :param objective: represents the function that should be optimized. Default is minimizing the difference between bin sums.
     :param time_limit: determines how much time (in seconds) the function should run before it stops. Default is infinity.
 
-    >>> from prtpy import BinsKeepingContents, BinsKeepingSums, printbins
-    >>> printbins(optimal(BinsKeepingContents(2), [4,5,6,7,8]))
+    >>> from prtpy import BinnerKeepingContents, BinnerKeepingSums, printbins
+    >>> printbins(optimal(BinnerKeepingContents(), 2, [4,5,6,7,8]))
     Bin #0: [4, 5, 6], sum=15.0
     Bin #1: [7, 8], sum=15.0
 
     The following examples are based on:
         Walter (2013), 'Comparing the minimum completion times of two longest-first scheduling-heuristics'.
     >>> walter_numbers = [46, 39, 27, 26, 16, 13, 10]
-    >>> printbins(optimal(BinsKeepingContents(3), walter_numbers))
+    >>> printbins(optimal(BinnerKeepingContents(), 3, walter_numbers))
     Bin #0: [16, 39], sum=55.0
     Bin #1: [13, 46], sum=59.0
     Bin #2: [10, 26, 27], sum=63.0
 
-    >>> optimal(BinsKeepingSums(5), items=[1, 2, 3, 4, 5])
+    >>> optimal(BinnerKeepingSums(), 5, items=[1, 2, 3, 4, 5])
     [1.0, 2.0, 3.0, 4.0, 5.0]
 
-    >>> optimal(BinsKeepingSums(5), items=[1,9,8,2,3,7,6,5,4])
+    >>> optimal(BinnerKeepingSums(), 5, items=[1,9,8,2,3,7,6,5,4])
     [9.0, 9.0, 9.0, 9.0, 9.0]
 
     Partitioning items with names:
@@ -66,11 +67,6 @@ def optimal(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, binner
     >>> partition(algorithm=optimal, numbins=2, items={"a":1, "b":2, "c":3, "d":3, "e":5, "f":9, "g":9}, outputtype=out.Sums)
     [16.0, 16.0]
     """
-    if binner is None: 
-        binner = bins.get_binner()
-    if numbins is None:
-        numbins = binner.numbins
-
     numitems = len(items)
     logger.info("\nComplete-Karmarkar-Karp Partitioning of %d items into %d parts.", numitems, numbins)
     items = sorted(items, reverse=True, key=binner.valueof)
@@ -121,31 +117,29 @@ def optimal(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, binner
     return best_partition_so_far
 
 
-
-def generator(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, best_difference_so_far:float = -np.inf, binner:Binner=None) -> Iterator[Bins]:
+def generator(
+    binner: Binner, numbins: int, items: List[any],
+    best_difference_so_far:float = -np.inf) -> Iterator[BinsArray]:
     """
     Iterator as mentioned in CKK algorithms which return better partition than the one found so far.
     best:  negative upper bound on the difference.
             -np.inf (default): yield better partitions than the one found so far.
            other: yield partitions with upper bound "best" on the difference.
 
-    >>> from prtpy import partition
-    >>> for part in generator(BinsKeepingSums(4), items=[1,2,3,3,5,9,9]): part
+    >>> from prtpy import partition, BinnerKeepingContents, BinnerKeepingSums
+    >>> for part in generator(BinnerKeepingSums(), 4, items=[1,2,3,3,5,9,9]): part
     [7.0, 7.0, 9.0, 9.0]
-    >>> for part in generator(BinsKeepingContents(4), items=[1, 3, 3, 4, 4, 5, 5, 5]): part
+    >>> for part in generator(BinnerKeepingContents(), 4, items=[1, 3, 3, 4, 4, 5, 5, 5]): part
     ([6.0, 8.0, 8.0, 8.0], [[1, 5], [4, 4], [3, 5], [3, 5]])
     """
-    if binner is None: 
-        binner = bins.get_binner()
-
     numitems = len(items)
-    logger.info("\nComplete-Karmarkar-Karp Partitioning of %d items into %d parts.", numitems, binner.numbins)
+    logger.info("\nComplete-Karmarkar-Karp Partitioning of %d items into %d parts.", numitems, numbins)
     items = sorted(items, reverse=True, key=binner.valueof)
 
     stack = []  #: List[List[Tuple[int, int, Bins, List[int]]]]
     first_heap = BinsSortedByMaxDiff(binner)
     for item in items:
-        new_bins = binner.add_item_to_bin(binner.new_bins(), item=item, bin_index=binner.numbins-1)
+        new_bins = binner.add_item_to_bin(binner.new_bins(numbins), item=item, bin_index=numbins-1)
         first_heap.push(new_bins)
     stack.append(first_heap)
 
@@ -160,7 +154,7 @@ def generator(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, best
         current_heap = stack.pop()
 
         # if could lead to better partition - maybe insert here upper bound constraint
-        lower_bound = _possible_partition_difference_lower_bound(current_heap, binner.numbins) 
+        lower_bound = _possible_partition_difference_lower_bound(current_heap, numbins) 
         if lower_bound <= best_difference_so_far:
             continue
 
@@ -202,7 +196,7 @@ def generator(bins: Bins, items: List[int],  valueof: Callable=lambda x: x, best
 
 if __name__ == '__main__':
     import doctest, sys
-    (failures, tests) = doctest.testmod(report=True)
+    (failures, tests) = doctest.testmod(report=True, optionflags=doctest.FAIL_FAST)
     print("{} failures, {} tests".format(failures, tests))
     if failures>0: 
         sys.exit(1)
