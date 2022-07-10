20a61a051a3f0841ab16ee740628ce49d8cbecf8
Erel Segal-Halevi <erelsgl@gmail.com>
2022-07-11 00:06:28 +0300

    greedy cflz binner

--- prtpy/packing/cflz_covering.py
+++ prtpy/packing/cflz_covering.py
@@ -24,37 +24,54 @@ def twothirds(
     Run the 2/3-approximation algorithm for bin covering.
     From Csirik et al (1999).
 
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> twothirds(BinsKeepingContents(0), 10, [11,12,13]).bins   # large items
-    [[13], [12], [11]]
-    >>> twothirds(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]).bins   # identical items
-    [[3, 3, 3, 3], [3, 3, 3, 3]]
-    >>> twothirds(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]).bins   # different items
-    [[10], [9, 1], [8, 2], [7, 3], [6, 4]]
-    >>> twothirds(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]).bins   # worst-case example (k=1)
-    [[994, 1, 1, 1, 1, 1, 1], [499, 499, 499], [499, 499, 499]]
-    >>> twothirds(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]).bins   # worst-case example (k=2)
-    [[988, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [499, 499, 499], [499, 499, 499], [499, 499, 499], [499, 499, 499]]
-    >>> twothirds(BinsKeepingContents(0), 1200, [594,594] + 12*[399] + 12*[1]).bins  # worst-case example for 3/4 (k=1)
-    [[594, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 399, 399], [594, 399, 399], [399, 399, 399, 399], [399, 399, 399, 399]]
+    >>> from prtpy import BinsKeepingContents, BinsKeepingSums, printbins
+    >>> printbins(twothirds(BinsKeepingContents(0), 10, [11,12,13]))   # large items
+    Bin #0: [13], sum=13.0
+    Bin #1: [12], sum=12.0
+    Bin #2: [11], sum=11.0
+    >>> printbins(twothirds(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]))   # identical items
+    Bin #0: [3, 3, 3, 3], sum=12.0
+    Bin #1: [3, 3, 3, 3], sum=12.0
+    >>> printbins(twothirds(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]))   # different items
+    Bin #0: [10], sum=10.0
+    Bin #1: [9, 1], sum=10.0
+    Bin #2: [8, 2], sum=10.0
+    Bin #3: [7, 3], sum=10.0
+    Bin #4: [6, 4], sum=10.0
+    >>> printbins(twothirds(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]))   # worst-case example (k=1)
+    Bin #0: [994, 1, 1, 1, 1, 1, 1], sum=1000.0
+    Bin #1: [499, 499, 499], sum=1497.0
+    Bin #2: [499, 499, 499], sum=1497.0
+    >>> printbins(twothirds(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]))   # worst-case example (k=2)
+    Bin #0: [988, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], sum=1000.0
+    Bin #1: [499, 499, 499], sum=1497.0
+    Bin #2: [499, 499, 499], sum=1497.0
+    Bin #3: [499, 499, 499], sum=1497.0
+    Bin #4: [499, 499, 499], sum=1497.0
+    >>> printbins(twothirds(BinsKeepingContents(0), 1200, [594,594] + 12*[399] + 12*[1]))  # worst-case example for 3/4 (k=1)
+    Bin #0: [594, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 399, 399], sum=1404.0
+    Bin #1: [594, 399, 399], sum=1392.0
+    Bin #2: [399, 399, 399, 399], sum=1596.0
+    Bin #3: [399, 399, 399, 399], sum=1596.0
     """
-    bins.add_empty_bins(1)
-    items = sorted(items, key=valueof, reverse=True)
+    binner = bins.get_binner()
+    bins = binner.new_bins(1)
+    items = sorted(items, key=binner.valueof, reverse=True)
     while len(items)>0:
         # Initialize with a single biggest item:
         biggest_item = items[0]
-        bins.add_item_to_bin(biggest_item, -1)
+        binner.add_item_to_bin(bins, biggest_item, -1)
         del items[0]
 
-        while len(items)>0 and bins.sums[-1]<binsize:
+        while len(items)>0 and binner.sums(bins)[-1]<binsize:
             # Fill with the smallest items in ascending order:
             smallest_item = items[-1]
-            bins.add_item_to_bin(smallest_item, -1)
+            binner.add_item_to_bin(bins, smallest_item, -1)
             del items[-1]
 
-        if bins.sums[-1]>=binsize:  # The current bin is full - add it and continue
-            bins.add_empty_bins(1)
-    bins.remove_bins(1) # remove the last, not-full bin.
+        if binner.sums(bins)[-1]>=binsize:  # The current bin is full - add it and continue
+            bins = binner.add_empty_bins(bins, 1)
+    bins = binner.remove_bins(bins, 1) # remove the last, not-full bin.
     return bins
 
 
@@ -69,24 +86,45 @@ def threequarters(
     Run the 3/4-approximation algorithm for bin covering.
     From Csirik et al (1999).
 
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> threequarters(BinsKeepingContents(0), 10, [11,12,13]).bins   # large items
-    [[13], [12], [11]]
-    >>> threequarters(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]).bins   # identical items
-    [[3, 3, 3, 3], [3, 3, 3, 3]]
-    >>> threequarters(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]).bins   # different items
-    [[10], [9, 1], [8, 2], [7, 3], [6, 5]]
-    >>> threequarters(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]).bins   # worst-case example for 2/3 (k=1)
-    [[499, 499, 1, 1], [499, 499, 1, 1], [499, 499, 1, 1]]
-    >>> threequarters(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]).bins   # worst-case example for 2/3 (k=2)
-    [[499, 499, 1, 1], [499, 499, 1, 1], [499, 499, 1, 1], [499, 499, 1, 1], [499, 499, 1, 1], [499, 499, 1, 1]]
-    >>> threequarters(BinsKeepingContents(0), 1200, [594,594] + 12*[399] + 12*[1]).bins   # worst-case example for 3/4 (k=1)
-    [[594, 594, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], [399, 399, 399, 399], [399, 399, 399, 399], [399, 399, 399, 399]]
-    >>> threequarters(BinsKeepingContents(0), 1000, [994, 501,501, 499,499,499,499]+12*[1]).bins
-    [[499, 499, 1, 1], [499, 499, 1, 1], [994, 1, 1, 1, 1, 1, 1], [501, 1, 1, 501]]
+    >>> from prtpy import BinsKeepingContents, BinsKeepingSums, printbins
+    >>> printbins(threequarters(BinsKeepingContents(0), 10, [11,12,13]))   # large items
+    Bin #0: [13], sum=13.0
+    Bin #1: [12], sum=12.0
+    Bin #2: [11], sum=11.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]))   # identical items
+    Bin #0: [3, 3, 3, 3], sum=12.0
+    Bin #1: [3, 3, 3, 3], sum=12.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]))   # different items
+    Bin #0: [10], sum=10.0
+    Bin #1: [9, 1], sum=10.0
+    Bin #2: [8, 2], sum=10.0
+    Bin #3: [7, 3], sum=10.0
+    Bin #4: [6, 5], sum=11.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]))   # worst-case example for 2/3 (k=1)
+    Bin #0: [499, 499, 1, 1], sum=1000.0
+    Bin #1: [499, 499, 1, 1], sum=1000.0
+    Bin #2: [499, 499, 1, 1], sum=1000.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]))   # worst-case example for 2/3 (k=2)
+    Bin #0: [499, 499, 1, 1], sum=1000.0
+    Bin #1: [499, 499, 1, 1], sum=1000.0
+    Bin #2: [499, 499, 1, 1], sum=1000.0
+    Bin #3: [499, 499, 1, 1], sum=1000.0
+    Bin #4: [499, 499, 1, 1], sum=1000.0
+    Bin #5: [499, 499, 1, 1], sum=1000.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 1200, [594,594] + 12*[399] + 12*[1]))   # worst-case example for 3/4 (k=1)
+    Bin #0: [594, 594, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], sum=1200.0
+    Bin #1: [399, 399, 399, 399], sum=1596.0
+    Bin #2: [399, 399, 399, 399], sum=1596.0
+    Bin #3: [399, 399, 399, 399], sum=1596.0
+    >>> printbins(threequarters(BinsKeepingContents(0), 1000, [994, 501,501, 499,499,499,499]+12*[1]))
+    Bin #0: [499, 499, 1, 1], sum=1000.0
+    Bin #1: [499, 499, 1, 1], sum=1000.0
+    Bin #2: [994, 1, 1, 1, 1, 1, 1], sum=1000.0
+    Bin #3: [501, 1, 1, 501], sum=1004.0
     """
-    bins.add_empty_bins(1)
-    items = sorted(items, key=valueof, reverse=True)
+    binner = bins.get_binner()
+    bins = binner.new_bins(1)
+    items = sorted(items, key=binner.valueof, reverse=True)
 
     big_items = [item for item in items if binsize/2 <= valueof(item)]  # X
     medium_items = [item for item in items if binsize/3 <= valueof(item) < binsize/2]  # Y
@@ -95,12 +133,12 @@ def threequarters(
     while True:
         if len(small_items)==0:
             # NOTE: We re-use the items remaining in the last bin.
-            decreasing_subroutine(bins, binsize, big_items, valueof)
-            decreasing_subroutine(bins, binsize, medium_items, valueof)
+            bins = decreasing_subroutine(binner, bins, binsize, big_items)
+            bins = decreasing_subroutine(binner, bins, binsize, medium_items)
             break
 
         elif len(big_items)==0 and len(medium_items)==0:
-            decreasing_subroutine(bins, binsize, small_items, valueof)
+            bins = decreasing_subroutine(binner, bins, binsize, small_items)
             break
 
         else:
@@ -110,29 +148,29 @@ def threequarters(
             biggest_medium_items = medium_items[0:2]   # It will be empty if Y is empty
             if sum(biggest_item) >= sum(biggest_medium_items):
                 for item in biggest_item:
-                    bins.add_item_to_bin(item, -1)
+                    binner.add_item_to_bin(bins, item, -1)
                     del big_items[0]
             else:
                 for item in biggest_medium_items:
-                    bins.add_item_to_bin(item, -1)
+                    binner.add_item_to_bin(bins, item, -1)
                     del medium_items[0]
 
-            while len(small_items)>0 and bins.sums[-1]<binsize:
+            while len(small_items)>0 and binner.sums(bins)[-1]<binsize:
                 # Fill with the smallest items in ascending order:
                 smallest_item = small_items[-1]
-                bins.add_item_to_bin(smallest_item, -1)
+                binner.add_item_to_bin(bins, smallest_item, -1)
                 del small_items[-1]
 
-            if bins.sums[-1]>=binsize:  # The current bin is full - add it and continue
-                bins.add_empty_bins(1)
+            if binner.sums(bins)[-1]>=binsize:  # The current bin is full - add it and continue
+                bins = binner.add_empty_bins(bins, 1)
 
-    bins.remove_bins(1)  # the last bin is not full - remove it
+    bins = binner.remove_bins(bins, 1)  # the last bin is not full - remove it
     return bins
 
 
 
 if __name__ == "__main__":
     import doctest
-    (failures,tests) = doctest.testmod(report=True)
+    (failures,tests) = doctest.testmod(report=True, optionflags=doctest.FAIL_FAST)
     print ("{} failures, {} tests".format(failures,tests))
 
--- prtpy/packing/greedy_covering.py
+++ prtpy/packing/greedy_covering.py
@@ -5,7 +5,7 @@ AUTHOR: Erel Segal-Halevi
 SINCE: 2021-04
 """
 
-from prtpy import outputtypes as out, Bins
+from prtpy import outputtypes as out, Bins, Binner, BinsArray
 from typing import Callable, List, Any
 
 
@@ -19,22 +19,34 @@ def decreasing(
     binsize: float,
     items: List[any],
     valueof: Callable[[Any], float] = lambda x: x,
-):
+)->BinsArray:
     """
     Run a simple bin-covering algorithm:
     it orders the items in descending order, and puts them into a bin until it is filled.
 
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> decreasing(BinsKeepingContents(0), 10, [11,12,13]).bins  # large items
-    [[13], [12], [11]]
-    >>> decreasing(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]).bins   # identical items
-    [[3, 3, 3, 3], [3, 3, 3, 3]]
-    >>> decreasing(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]).bins   # different items
-    [[10], [9, 8], [7, 6], [5, 4, 3]]
-    >>> decreasing(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]).bins   # worst-case example (k=1)
-    [[994, 499], [499, 499, 499], [499, 499, 1, 1]]
-    >>> decreasing(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]).bins   # worst-case example (k=2)
-    [[988, 499], [499, 499, 499], [499, 499, 499], [499, 499, 499], [499, 499, 1, 1]]
+    >>> from prtpy import BinsKeepingContents, BinsKeepingSums, printbins
+    >>> printbins(decreasing(BinsKeepingContents(0), 10, [11,12,13]))  # large items
+    Bin #0: [13], sum=13.0
+    Bin #1: [12], sum=12.0
+    Bin #2: [11], sum=11.0
+    >>> printbins(decreasing(BinsKeepingContents(0), 10, [3,3,3,3, 3,3,3,3, 3,3,3]))   # identical items
+    Bin #0: [3, 3, 3, 3], sum=12.0
+    Bin #1: [3, 3, 3, 3], sum=12.0
+    >>> printbins(decreasing(BinsKeepingContents(0), 10, [1,2,3,4,5,6,7,8,9,10]))   # different items
+    Bin #0: [10], sum=10.0
+    Bin #1: [9, 8], sum=17.0
+    Bin #2: [7, 6], sum=13.0
+    Bin #3: [5, 4, 3], sum=12.0
+    >>> printbins(decreasing(BinsKeepingContents(0), 1000, [994, 499,499,499,499,499,499, 1,1,1,1,1,1]))   # worst-case example (k=1)
+    Bin #0: [994, 499], sum=1493.0
+    Bin #1: [499, 499, 499], sum=1497.0
+    Bin #2: [499, 499, 1, 1], sum=1000.0
+    >>> printbins(decreasing(BinsKeepingContents(0), 1000, [988] + 12*[499] + 12*[1]))   # worst-case example (k=2)
+    Bin #0: [988, 499], sum=1487.0
+    Bin #1: [499, 499, 499], sum=1497.0
+    Bin #2: [499, 499, 499], sum=1497.0
+    Bin #3: [499, 499, 499], sum=1497.0
+    Bin #4: [499, 499, 1, 1], sum=1000.0
 
     >>> from prtpy import pack
     >>> pack(algorithm=decreasing, binsize=60, items={"a":44, "b":24, "c":24, "d":22, "e":21, "f":17, "g":8, "h":8, "i":6, "j":6})
@@ -42,32 +54,34 @@ def decreasing(
     >>> pack(algorithm=decreasing, binsize=60, items={"a":44, "b":24, "c":24, "d":22, "e":21, "f":17, "g":8, "h":8, "i":6, "j":6}, outputtype=out.Sums)
     [68.0, 67.0]
     """
-    bins.add_empty_bins(1)
-    items = sorted(items, key=valueof, reverse=True)
-    decreasing_subroutine(bins, binsize, sorted_items=items, valueof=valueof)
-    bins.remove_bins(1)  # the last bin is not full - remove it
+    binner = bins.get_binner()
+    bins = binner.new_bins(1)
+    sorted_items = sorted(items, key=binner.valueof, reverse=True)
+    bins = decreasing_subroutine(binner, bins, binsize, sorted_items)
+    bins = binner.remove_bins(bins, 1)  # the last bin is not full - remove it
     return bins
 
 
 def decreasing_subroutine(
-    bins: Bins, 
+    binner: Binner,
+    bins: BinsArray, 
     binsize: float,
     sorted_items: List[any],
-    valueof: Callable[[Any], float]
-):
+)->BinsArray:
     """
     Run a simple bin-covering algorithm:
     it orders the items in descending order, and puts them into a bin until it is filled.
-    This is a subroutine that accepts an already-initialized Bins structure, and already-sorted items list.
+    This is a subroutine that accepts an already-initialized Bins Array, and already-sorted items list.
     """
     for item in sorted_items:
-        bins.add_item_to_bin(item, -1)
-        if bins.sums[-1] >= binsize: # the last bin is full - open a new one
-            bins.add_empty_bins(1)
+        binner.add_item_to_bin(bins, item, -1)
+        if binner.sums(bins)[-1] >= binsize: # the last bin is full - open a new one
+            bins = binner.add_empty_bins(bins, 1)
+    return bins
 
 
 if __name__ == "__main__":
     import doctest
-    (failures,tests) = doctest.testmod(report=True)
+    (failures,tests) = doctest.testmod(report=True, optionflags=doctest.FAIL_FAST)
     print ("{} failures, {} tests".format(failures,tests))
 
