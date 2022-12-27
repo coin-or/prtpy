8ba9a916952ea2d246d7aa203164e8181e2ab633
yoellchemla <ychemla96@gmail.com>
2022-12-28 01:57:57 +0200

    implement algorithm, update test

--- prtpy/partitioning/Horowitz_And_Shani.py
+++ prtpy/partitioning/Horowitz_And_Shani.py
@@ -1,43 +1,114 @@
 import doctest
-
 from numpy import number
 
+# credit: https://sites.cs.queensu.ca/courses/cisc365/Labs/Week%204/2019%20Week%204%20+%205%20Lab.pdf
 
-def horowitz_sahni(lst: list):
+# help functions:
+
+
+def Pair_Sum(v1, v2, k):
+    """
+    divide to pair
+
+    """
+    pair1 = 0
+    pair2 = (len(v2)-1)
+
+    while pair1 <= (len(v1) - 1) and (pair2 >= 0):
+        ans = v1[pair1] + v2[pair2]
+        t = sum(ans)
+        if t < k:
+            pair1 = pair1 + 1
+        elif t == k:
+            return pair1, pair2
+        else:
+            pair2 = pair2 - 1
+    return None
+
+
+def generate_subset_sum(s):
+    """"
+    create subset sum
+    """
+    sets = [[]]
+    for i in range(0, len(s)):
+        length_sets = len(sets)
+        for j in range(0, length_sets):
+            t = sets[j] + [s[i]]
+            sets.append(t)  # add t in the set
+    return sets
+
+
+def compute_each_subset_sum(s, k):
+    """
+    compute the sum of each subset sum
+    """
+    sets = [[]]
+    for i in range(0, len(s)):  # create a new set for each one
+        for j in range(0, len(sets)):
+            t = sets[j] + [s[i]]
+            sets.append(t)
+            if sum(t) == k:  # compare the sum to the target k
+                return t
+    return None  # no found
+
+
+def horowitz_sahni(s, k):
     """
     Optimally Scheduling Small Numbers of Identical Parallel Machines,
     by ichard E.Korf and Ethan L. Schreiber (2013) https://ojs.aaai.org/index.php/ICAPS/article/view/13544
     Algorithm 1: get a list, return the pair with the sum within the complete sum, if don't exit raise an error.
     (part all the subset sum and check relative to the complete sum).
 
-    >>> horowitz_sahni([3, 1, 2, 2])
-    [3, 1] # 4
+    >>> horowitz_sahni([3, 5, 3, 9, 18, 4, 5, 6], 28)
+    [18, 4, 6]
 
-    >>> horowitz_sahni([9, 8, 7])
-    Traceback (most recent call last):
-        ...
-    Exception: Exception
+    >>> horowitz_sahni([1, 2, 3, 4, 5], 5)
+    [5]
 
-    >>> horowitz_sahni([100, 20 ,30])
-    Traceback (most recent call last):
-        ...
-    Exception: Exception
+    >>> horowitz_sahni([1, 2, 3, 4, 5], 9)
+    [4, 5]
 
-    >>> horowitz_sahni([10, 10, 10, 10] )
-     [10, 10] # 20
-
-    >>> horowitz_sahni([0, 2, 4, 3, 1])
-    [2,3] # 5
-
-    >>> horowitz_sahni([2, 4, 6, 8, 10])
-    Traceback (most recent call last):
-        ...
-    Exception: Exception
+    >>> horowitz_sahni([-1, -2, -3, -4], -3)
+    [-3]
 
     """
+    left_sum = s[:len(s) // 2]
+    right_sum = s[len(s) // 2:]
 
-    return number   # Empty implementation
+    # send to help function and create list of the left and right sum
+    list_of_right_sum = generate_subset_sum(right_sum)
+    list_of_left_sum = generate_subset_sum(left_sum)
+
+    # Checks the sums if over than target value:
+
+    #     right side
+    for i in list_of_right_sum:
+        if sum(i) == k:
+            return i  # equal to the target
+
+    # left side
+    for i in list_of_left_sum:
+        if sum(i) == k:
+            return i   # equal to the target
+
+    # if the sums don't over than target value, sort lists to pair_sum function:
+
+    # right side
+    list_of_right_sum.sort()
+
+    # left side
+    list_of_left_sum.sort()
+
+    # call the help function
+    if Pair_Sum(list_of_right_sum, list_of_left_sum, k) is not None:
+        print(Pair_Sum(list_of_right_sum, list_of_left_sum, k))
+    else:
+        return None
 
 
 if __name__ == '__main__':
     doctest.testmod()
+    # arr = [3, 5, 3, 9, 18, 4, 5, 6]
+    # n = 11
+    # print(horowitz_sahni(arr, n))  # [3, 5, 3]
--- tests/test_horowoitz_and_shani.py
+++ tests/test_horowoitz_and_shani.py
@@ -7,66 +7,66 @@ class TestMain(unittest.TestCase):
     def test_hs(self):
         # default case
         arr0 = []
-        self.assertEqual(horowitz_sahni(arr0), [])
+        self.assertEqual(horowitz_sahni([], 0), [], "good")
 
-        arr0_1 = [3, 1, 2, 2]
-        self.assertEqual(horowitz_sahni(arr0_1), [3, 1])
+        arr0_1 = [1, 2, 3, 4, 5]
+        self.assertEqual(horowitz_sahni(arr0_1, 5), [5], "good")
 
         arr0_2 = [1, 2, 3, 4, 5, 6, 7]
-        self.assertEqual(horowitz_sahni(arr0_2), [7, 4, 3])
+        self.assertEqual(horowitz_sahni(arr0_2, 13), [6, 7], "good")
 
         arr0_3 = [0, 0, 0, 0]
-        self.assertEqual(horowitz_sahni(arr0_3), [0, 0])
+        self.assertEqual(horowitz_sahni(arr0_3, 0), [], "good")
 
         arr0_4 = [1, 1, 1, 1]
-        self.assertEqual(horowitz_sahni(arr0_4), [1, 1])
+        self.assertEqual(horowitz_sahni(arr0_4, 2), [1, 1], "good")
 
         arr0_5 = [30, 30, 30, 30]
-        self.assertEqual(horowitz_sahni(arr0_5), [30, 30])
+        self.assertEqual(horowitz_sahni(arr0_5, 60), [30, 30], "good")
 
         arr0_6 = [30, 20, 10, 0]
-        self.assertEqual(horowitz_sahni(arr0_6), [30])
+        self.assertEqual(horowitz_sahni(arr0_6, 50), [30, 20], "good")
 
         arr0_7 = [3, 5, 7, 9, 11, 13]  # odd
-        self.assertEqual(horowitz_sahni(arr0_7), [11, 13])
+        self.assertEqual(horowitz_sahni(arr0_7, 24), [11, 13], "good")
 
         arr0_8 = [2, 4, 6, 8]  # even
-        self.assertEqual(horowitz_sahni(arr0_8), [2, 8])
+        self.assertEqual(horowitz_sahni(arr0_8, 6), [6], "good")
 
-    # negative number
+    # # negative number
 
         arr1 = [-1, -2, -3, -4]
-        self.fail(arr1, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr1, 3), [-1], "Exception")
 
         arr1_1 = [1, 2, 3, 4, 5, -6]
-        self.fail(arr1_1, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr1_1, 3), [1], "Exception")
 
         arr1_2 = [-1, 0, -2, 3]  # sum 0
-        self.fail(arr1_2, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr1_2, 3), [0], "Exception")
 
-        # non integer value
+    #     # non integer value
 
         arr2 = [1, 2, 3, 4, 5, 5.5]
-        self.fail(arr2, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr2, 7), [5.5], "Exception")
 
         arr2_1 = [1.0, 2.0, 3.0, 4.0]
-        self.fail(arr2_1, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr2_1, 3), [2.0, 1.0], "Exception")
 
         arr2_2 = [1, 2, 3, -4.0]
-        self.fail(arr2_2, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr2_2, 6), [1, 2, 3], "Exception")
 
         arr2_3 = [-1.00, -2.2, 3.5, 4.7]
-        self.fail(arr2_3, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr2_3, 6), [-2.2, 3.5, 4.7], "Exception")
 
         arr2_4 = [0.5, 0.4, 0.3]
-        self.fail(arr2_4, "Exception")
+        self.assertNotEqual(horowitz_sahni(arr2_4, 1.2), [0.5, 0.4, 0.3], "Exception")
 
-    #   a large numbers
+    # #   a large numbers
         arr3 = [1000, 2000, 30000, 2000, 65000, 100000]
-        self.assertEqual(horowitz_sahni(arr3), [1000, 2000, 30000, 2000, 65000])
+        self.assertEqual(horowitz_sahni(arr3, 67000), [2000, 65000], "good")
 
-        arr3_1 = [10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000]
-        self.assertEqual(horowitz_sahni(arr3), [10000000000, 10000000000, 10000000000, 10000000000])
+        arr3_1 = [10000000000000000000000000000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000]
+        self.assertEqual(horowitz_sahni(arr3_1, 10000000000000000000000000000000000), [10000000000000000000000000000000000], "good")
 
 
 if __name__ == '__main__':
