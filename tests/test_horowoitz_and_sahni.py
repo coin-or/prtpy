d0103d156362f1fdbfba9da40f0fd34810c29c24
yoelchemla <74509202+yoelchemla@users.noreply.github.com>
2023-01-12 15:27:36 +0200

    update large input

--- tests/test_horowoitz_and_sahni.py
+++ tests/test_horowoitz_and_sahni.py
@@ -38,10 +38,10 @@ class TestMain(unittest.TestCase):
         arr0_8 = [2, 4, 6, 8]  # even
         self.assertEqual(Horowitz_Sahni(arr0_8, 6), [6], "good")
 
-    # # negative number
+    # negative number
 
         arr1 = [-1, -2, -3, -4]
-        self.assertNotEqual(Horowitz_Sahni(arr1, 3), [-1], "Exception")
+        self.assertNotEqual(Horowitz_Sahni(arr1, 3), [-1, -3], "Exception")
 
         arr1_1 = [1, 2, 3, 4, 5, -6]
         self.assertNotEqual(Horowitz_Sahni(arr1_1, 3), [1], "Exception")
@@ -49,7 +49,7 @@ class TestMain(unittest.TestCase):
         arr1_2 = [-1, 0, -2, 3]  # sum 0
         self.assertNotEqual(Horowitz_Sahni(arr1_2, 3), [0], "Exception")
 
-    #     # non integer value
+    # non integer value
 
         arr2 = [1, 2, 3, 4, 5, 5.5]
         self.assertNotEqual(Horowitz_Sahni(arr2, 7), [5.5], "Exception")
@@ -58,20 +58,28 @@ class TestMain(unittest.TestCase):
         self.assertNotEqual(Horowitz_Sahni(arr2_1, 3), [2.0, 1.0], "Exception")
 
         arr2_2 = [1, 2, 3, -4.0]
-        self.assertNotEqual(Horowitz_Sahni(arr2_2, 6), [1, 2, 3], "Exception")
+        self.assertNotEqual(Horowitz_Sahni(arr2_2, 6), [1, 2, 3, -4.0], "Exception")
 
         arr2_3 = [-1.00, -2.2, 3.5, 4.7]
-        self.assertNotEqual(Horowitz_Sahni(arr2_3, 6), [-2.2, 3.5, 4.7], "Exception")
+        self.assertNotEqual(Horowitz_Sahni(arr2_3, 6), [-1.00, 3.5, 4.7], "Exception")
 
         arr2_4 = [0.5, 0.4, 0.3]
         self.assertNotEqual(Horowitz_Sahni(arr2_4, 1.2), [0.5, 0.4, 0.3], "Exception")
 
-    # #   a large numbers
+    #   a large numbers
         arr3 = [1000, 2000, 30000, 2000, 65000, 100000]
         self.assertEqual(Horowitz_Sahni(arr3, 67000), [2000, 65000], "good")
 
         arr3_1 = [10000000000000000000000000000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000, 10000000000]
         self.assertEqual(Horowitz_Sahni(arr3_1, 10000000000000000000000000000000000), [10000000000000000000000000000000000], "good")
+
+        arr3_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        self.assertEqual(Horowitz_Sahni(arr3_2, 1000), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                                        12, 13, 14, 15, 16, 17, 18, 19, 20], "good")
+
+        arr3_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+        self.assertNotEqual(Horowitz_Sahni(arr3_2, 100), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                                                          12, 13, 14, 15, 16, 17, 18, 19, 20], "Exception")
 
 
 if __name__ == '__main__':
