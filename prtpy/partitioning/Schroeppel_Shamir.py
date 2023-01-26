b4e511be71e29afe95a255c60861c96b168fedab
yoelchemla <74509202+yoelchemla@users.noreply.github.com>
2023-01-26 19:54:15 +0200

    Update Schroeppel_Shamir.py

--- prtpy/partitioning/Schroeppel_Shamir.py
+++ prtpy/partitioning/Schroeppel_Shamir.py
@@ -3,8 +3,9 @@ Optimally Scheduling Small Numbers of Identical Parallel Machines,
 by ichard E.Korf and Ethan L. Schreiber (2013) https://ojs.aaai.org/index.php/ICAPS/article/view/13544
 Yoel Chemla
 """
+import copy
 import doctest
-
+import heapq
 from prtpy.partitioning.MaxHeap import MaxHeap
 from prtpy.partitioning.MinHeap import MinHeap
 from prtpy.partitioning.Horowitz_And_Sahni import poewer_set
@@ -17,8 +18,16 @@ def schroeppel_shamir(s):
 
         >>> schroeppel_shamir([1, 2, 3])
         ([[0, 0], [0, 1]], [[2, 3], [2, 0], [0, 3], [0, 0]])
-    """
 
+        >>> schroeppel_shamir([100, 200, 100, 500])
+        ([[0, 0], [0, 200], [100, 0], [100, 200]], [[100, 500], [100, 0], [0, 500], [0, 0]])
+
+        >>> schroeppel_shamir([])
+        ([[0, 0]], [[0, 0]])
+
+        >>> schroeppel_shamir([5, 6, 7])
+        ([[0, 0], [0, 5]], [[6, 7], [6, 0], [0, 7], [0, 0]])
+    """
     # sort s in decrease order
     sorted(s, reverse=True)
 
@@ -32,21 +41,17 @@ def schroeppel_shamir(s):
     a1 = left_sum[len(left_sum) // 2:]
     b0 = right_sum[:len(right_sum) // 2]
     b1 = right_sum[len(right_sum) // 2:]
-    
+ 
     subset_sum_a0 = poewer_set(a0)
     subset_sum_a1 = poewer_set(a1)
     subset_sum_b0 = poewer_set(b0)
     subset_sum_b1 = poewer_set(b1)
    
-
     list_of_sum_a0 = [sum(subset_sum_a0[i]) for i in range(len(subset_sum_a0))]
-
     list_of_sum_a1 = [sum(subset_sum_a1[i]) for i in range(len(subset_sum_a1))]
-
     list_of_sum_b0 = [sum(subset_sum_b0[i]) for i in range(len(subset_sum_b0))]
-
     list_of_sum_b1 = [sum(subset_sum_b1[i]) for i in range(len(subset_sum_b1))]
-
+    
     #  increase
     list_of_sum_a0.sort()
     list_of_sum_a1.sort()
@@ -54,21 +59,33 @@ def schroeppel_shamir(s):
     #  decrease
     list_of_sum_b0.sort(reverse=True)
     list_of_sum_b1.sort(reverse=True)
-
+    
     # merge
     list_sorted1 = [[list_of_sum_a0[i], list_of_sum_a1[j]] for i in range(len(list_of_sum_a0)) for j in range(len(list_of_sum_a1))]
-    
-    min_heap = MinHeap()
+    min_heap = MinHeap() # create a new min heap
     for i in list_sorted1:
         min_heap.push(i)
- 
+    
+    #deep copy
+    deep_copied_heap = copy.deepcopy(min_heap)
+    
+    #empty the original heap
+    while len(min_heap.heap) is not 0:
+        min_heap.pop()
+    
     list_sorted2 = [[list_of_sum_b0[i], list_of_sum_b1[j]] for i in range(len(list_of_sum_b0)) for j in range(len(list_of_sum_b1))]
-    max_heap = MaxHeap()
+    max_heap = MaxHeap() # create a new max heap
     for i in list_sorted2:
         max_heap.push(i)
+    
+    #deep copy
+    deep_copied_heap2 = copy.deepcopy(max_heap)
 
-    return min_heap.heap, max_heap.heap
+    #empty the original heap
+    while len(max_heap.heap) is not 0:
+        max_heap.pop()
+    return deep_copied_heap.heap, deep_copied_heap2.heap
 
 
 if __name__ == '__main__':
-    doctest.testmod()
+    # doctest.testmod()
