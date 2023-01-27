0c5ad7dbcecd2bd3191f023e2af2e42db00b1638
Erel Segal-Halevi <erelsgl@gmail.com>
2023-01-27 04:36:40 +0200

    examples for FFD

--- examples/packing_algorithms.py
+++ examples/packing_algorithms.py
@@ -16,3 +16,9 @@ print(prtpy.pack(algorithm=prtpy.packing.first_fit_decreasing, binsize=61, items
 
 #' The advanced Bin Completion algorithm; programmed by Avshalom and Tehilla
 print(prtpy.pack(algorithm=prtpy.packing.bin_completion, binsize=61, items=items))
+
+#' More FFD examples from a recent paper:
+itemsA = [51, 28, 27, 27, 27, 26, 12, 12, 11, 11, 11, 11, 11, 11, 10]
+print("A: ",prtpy.pack(algorithm=prtpy.packing.first_fit_decreasing, binsize=75, items=itemsA, outputtype=prtpy.out.PartitionAndSums))
+itemsB = [51, 28, 27, 27, 27, 24, 21, 20, 10, 10, 10, 9, 9, 9, 9]
+print("B: ",prtpy.pack(algorithm=prtpy.packing.first_fit_decreasing, binsize=75, items=itemsB, outputtype=prtpy.out.PartitionAndSums))
