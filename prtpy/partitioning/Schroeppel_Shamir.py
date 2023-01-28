85abe3d33e95f5c2371e55299e10258bd3421e8a
yoelchemla <74509202+yoelchemla@users.noreply.github.com>
2023-01-28 23:14:21 +0200

    Update Schroeppel_Shamir.py

--- prtpy/partitioning/Schroeppel_Shamir.py
+++ prtpy/partitioning/Schroeppel_Shamir.py
@@ -15,16 +15,12 @@ def schroeppel_shamir(s):
     """
         Algorithm 2: get a list, return list that contain all the pair from the groups partiroins.
         This algorithm help to algorithm1 with the partition.
-
         >>> schroeppel_shamir([1, 2, 3])
         ([[0, 0], [0, 1]], [[2, 3], [2, 0], [0, 3], [0, 0]])
-
         >>> schroeppel_shamir([100, 200, 100, 500])
         ([[0, 0], [0, 200], [100, 0], [100, 200]], [[100, 500], [100, 0], [0, 500], [0, 0]])
-
         >>> schroeppel_shamir([])
         ([[0, 0]], [[0, 0]])
-
         >>> schroeppel_shamir([5, 6, 7])
         ([[0, 0], [0, 5]], [[6, 7], [6, 0], [0, 7], [0, 0]])
     """
@@ -41,17 +37,17 @@ def schroeppel_shamir(s):
     a1 = left_sum[len(left_sum) // 2:]
     b0 = right_sum[:len(right_sum) // 2]
     b1 = right_sum[len(right_sum) // 2:]
- 
+
     subset_sum_a0 = poewer_set(a0)
     subset_sum_a1 = poewer_set(a1)
     subset_sum_b0 = poewer_set(b0)
     subset_sum_b1 = poewer_set(b1)
-   
+
     list_of_sum_a0 = [sum(subset_sum_a0[i]) for i in range(len(subset_sum_a0))]
     list_of_sum_a1 = [sum(subset_sum_a1[i]) for i in range(len(subset_sum_a1))]
     list_of_sum_b0 = [sum(subset_sum_b0[i]) for i in range(len(subset_sum_b0))]
     list_of_sum_b1 = [sum(subset_sum_b1[i]) for i in range(len(subset_sum_b1))]
-    
+
     #  increase
     list_of_sum_a0.sort()
     list_of_sum_a1.sort()
@@ -59,29 +55,31 @@ def schroeppel_shamir(s):
     #  decrease
     list_of_sum_b0.sort(reverse=True)
     list_of_sum_b1.sort(reverse=True)
-    
+
     # merge
-    list_sorted1 = [[list_of_sum_a0[i], list_of_sum_a1[j]] for i in range(len(list_of_sum_a0)) for j in range(len(list_of_sum_a1))]
-    min_heap = MinHeap() # create a new min heap
+    list_sorted1 = [[list_of_sum_a0[i], list_of_sum_a1[j]] for i in range(len(list_of_sum_a0)) for j in
+                    range(len(list_of_sum_a1))]
+    min_heap = MinHeap()  # create a new min heap
     for i in list_sorted1:
         min_heap.push(i)
-    
-    #deep copy
+
+    # deep copy
     deep_copied_heap = copy.deepcopy(min_heap)
-    
-    #empty the original heap
+
+    # empty the original heap
     while len(min_heap.heap) is not 0:
         min_heap.pop()
-    
-    list_sorted2 = [[list_of_sum_b0[i], list_of_sum_b1[j]] for i in range(len(list_of_sum_b0)) for j in range(len(list_of_sum_b1))]
-    max_heap = MaxHeap() # create a new max heap
+
+    list_sorted2 = [[list_of_sum_b0[i], list_of_sum_b1[j]] for i in range(len(list_of_sum_b0)) for j in
+                    range(len(list_of_sum_b1))]
+    max_heap = MaxHeap()  # create a new max heap
     for i in list_sorted2:
         max_heap.push(i)
-    
-    #deep copy
+
+    # deep copy
     deep_copied_heap2 = copy.deepcopy(max_heap)
 
-    #empty the original heap
+    # empty the original heap
     while len(max_heap.heap) is not 0:
         max_heap.pop()
     return deep_copied_heap.heap, deep_copied_heap2.heap
